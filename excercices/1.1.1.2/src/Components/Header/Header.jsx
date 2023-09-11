58a907b2367ea82ace1b36cd0ff9a957882a9a96
adam <adame.barhdadi@student.vinci.be>
2023-09-11 14:23:19 +0200

    1.1-1.2 : import image

--- excercices/1.1.1.2/src/Components/Header/Header.jsx
+++ excercices/1.1.1.2/src/Components/Header/Header.jsx
@@ -1,6 +1,12 @@
+import logoHe from './LOGO HE VINCI.png'
+
 const Header = (props)=>{
     return (
+      <div>
+      <a href="https://e-vinci.github.io/web3/modules/1"><img src = {logoHe}></img></a>
       <h1>{props.name}</h1>
+      </div>
+ 
     )
     }
 
