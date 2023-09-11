165a1105e4f27b621f5cbf1ff6ac1391ef237453
adam <adame.barhdadi@student.vinci.be>
2023-09-11 12:48:35 +0200

    part1

---
+++ excercices/1.1-1.2/src/App.jsx
@@ -0,0 +1,37 @@
+const Header = (props)=>{
+return (
+  <h1>{props.name}</h1>
+)
+}
+const Content = (props)=> {
+return(
+  <p>{props.part} {props.exercice}</p>
+)
+}
+const Total = (props)=> {
+  return(
+    <p>Number of exercises {props.cmpt}</p>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header name = {course} />
+      <Content part={part1} exercice = {exercises1} />
+      <Content part={part2} exercice = {exercises2} />
+      <Content part={part3} exercice = {exercises3} />
+      <Total cmpt={exercises1 + exercises2 + exercises3}/>
+
+    </div>
+  )
+}
+
+export default App
