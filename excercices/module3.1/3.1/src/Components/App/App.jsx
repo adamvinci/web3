7819907aebe41f0e4b7cd1616cb23d9984b4ec98
adam <adame.barhdadi@student.vinci.be>
2023-09-13 13:25:23 +0200

    module3 : loading

--- excercices/module3.1/3.1/src/Components/App/App.jsx
+++ excercices/module3.1/3.1/src/Components/App/App.jsx
@@ -1,26 +1,35 @@
 import ButtonFeedback from "../ButtonFeedback/ButtonFeedback"
 import DisplayStatistic from "../DisplayStatisic/DisplayStatisic"
 import { useState } from "react"
+import Loading from "../Loading/Loading"
 
 
 const App = () => {
 
     const [ clicks, setClicks ] = useState({good :0,bad:0,neutral:0})
+    const [ loading, setLoading ] = useState(true)
 
     const handlerGoodClicks = ()=> setClicks({...clicks,good:clicks.good+1})
     const handlerBadClicks = ()=> setClicks({...clicks,bad:clicks.bad+1})
     const handlerNeutralClicks = ()=> setClicks({...clicks,neutral:clicks.neutral+1})
+    setTimeout(() => setLoading(false), 3000);
 
   
     return (
-      <div id="cmp">
-  
+      <div id="cmp">    
+        {loading ? (<Loading />):(
+         <>
         <h1>Give feedback</h1>
         <ButtonFeedback handler={handlerGoodClicks} text = 'good'/>
         <ButtonFeedback  handler={handlerNeutralClicks} text = 'neutral'/>
         <ButtonFeedback  handler={handlerBadClicks} text = 'bad'/>
         <DisplayStatistic clicks={clicks}/>
-      </div>
-    )
+        </>
+      
+    )}
+      </div>    
+    );
+    
+
   }
 export default App
